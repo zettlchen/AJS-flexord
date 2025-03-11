f56eb973785a2f814954dbfa538fd70c65a5366a
zettlchen <zettlchen@gmx.at>
2025-03-11 11:19:19 +0100

    fixup of visualization script

--- scripts/visualization.R
+++ scripts/visualization.R
@@ -1,32 +1,31 @@
 library(dplyr)
 library(ggplot2)
 
-algo_levels <- c("kmeans", "kmodes", "kmedians", "kgower", "kGDM2",
-                 "PAM:Gower", "PAM:GDM2",
+algo_levels <- c("kmeans", "kmodes", "kmedians", "kgower", "kgowerMed", "kGDM2centOpt",
+                 "kGDM2centMode", "PAM:Gower", "PAM:GDM2",
                  "regularized normal", "multinomial", "binomial", "betabinomial")
-algo_labels <- c("kmeans", "kmodes", "kmedians", "kGower", "kGDM2",
-                 "PAM:Gower", "PAM:GDM2",
+algo_labels <- c("kmeans", "kmodes", "kmedians", "kGowercentMin", "kGowercentMedian", "kGDM2centOpt",
+                 "kGDM2centMode", "PAM:Gower", "PAM:GDM2",
                  "regularized normal", "multinomial", "binomial", "beta-binomial")
 sim = Sys.glob("../data/sim_backpain_sample_size_ncat_*_alpha*.rds") %>%
   sapply(readRDS, simplify = F) %>%
-  setNames(gsub('\\D|2', '', names(.))) %>% 
+  setNames(gsub('\\D|2', '', names(.))) %>% #extracting info an alphas (2 is necessary for GDM*2*)
   bind_rows(.id='alpha') |> 
-  filter(algo != 'kGDM2centOpt') |> #removing it as it is unfinished (30.10.24)
-  mutate(algo=gsub('centMode', '', algo)) |> #renaming, same reason as above
   mutate(algo=gsub('om$', 'omial', algo),
-         algo=gsub('regnorm', 'regularized normal', algo)) |> #also, renaming algos
+         algo=gsub('regnorm', 'regularized normal', algo)) |> #renaming algos
   mutate(alpha=as.factor(as.numeric(alpha)),
          `response level length r`=as.factor(size+1),
          scale_treatment = case_when(algo %in% c('kmodes', 'multinomial') ~ 'nominalization',
                                      algo %in% c('regularized normal', 'kmeans') ~ 'numerical coding',
                                      TRUE ~ 'respecting ordinal scales'),
-         scale_treatment=factor(scale_treatment,
-                                levels=c('numerical coding',
+         scale_treatment = factor(scale_treatment,
+                                  levels=c('numerical coding',
                                          'nominalization',
                                          'respecting ordinal scales')),
-         type=factor(ifelse(algo %in% c('betabinomial', 'binomial', 'multinomial',
-                                        'regularized normal'), 'model-based',
-                            'partitioning'), levels=c('partitioning', 'model-based'))) |>
+         type = case_when(algo %in% c('betabinomial', 'binomial', 'multinomial',
+                                      'regularized normal') ~ 'model-based',
+                          TRUE ~ 'partitioning'),
+         type = factor(type, levels = c('partitioning', 'model-based'))) |>
   arrange(type, scale_treatment) |> 
   mutate(method = factor(algo, levels = algo_levels, labels = algo_labels)) |>
   rename(m=n_vars, ARI=ari) |> 
@@ -34,7 +33,7 @@ sim = Sys.glob("../data/sim_backpain_sample_size_ncat_*_alpha*.rds") %>%
 
 # ws <- wesanderson::wes_palette('Moonrise3', n=3) |> setNames(c('pink', 'brown', 'turquoise'))
 ws_expanded <- list(
-  pink = c('#F0808C', '#F4B5BD'),#'#D56B75'),
+  pink = c('#F0808C', '#F4B5BD'),
   brown = c('#8E8540', '#B5AF69'),
   turquoise = c('#43CD80', '#8EE5EE', '#A3E3ED', '#00688B', '#7BC9D6', '#74C8B1', '#60BCCC', '#76EEC6', '#3F8A7C', '#357885', '#4BA39F')
 )
